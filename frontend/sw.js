6c3664ad3f1b56c2eef4a7b16c0096d708a15fc2
KuetTai <kt.xtrik@gmail.com>
2026-06-03 23:40:15 +0800

    fix: bump SW cache to v11 to clear stale header

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rlc-cafe-v10';
+const CACHE_NAME = 'rlc-cafe-v11';
 const SHELL = [
   './', './index.html', './track.html', './pos.html', './admin.html',
   './css/style.css', './css/admin.css',
