ee3e9111cfa878d2349ff754f0ae703472bc375a
KuetTai <kt.xtrik@gmail.com>
2026-06-03 16:30:31 +0800

    Add custom domain support for 153.oasisofcares.org
    - Add CNAME file for GitHub Pages custom domain
    - Make BASE_PATH auto-detect (github.io vs custom domain)
    - Update manifest to use relative paths

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,2 @@
 const API_BASE = 'https://hcydppml1a.execute-api.ap-southeast-5.amazonaws.com/prod';
-const BASE_PATH = '/rlc-cafe-pos';
+const BASE_PATH = window.location.hostname.includes('github.io') ? '/rlc-cafe-pos' : '';
